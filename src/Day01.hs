75ab33a13721cdff9292c65b6a1e05c1cac91c80
mru2 <david.ruyer+github@gmail.com>
2020-12-01 20:58:27 +0100

    day1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,16 +1,17 @@
 module Day01 where
 
 import Common
+import Data.List
 
 solution =
-    Solution
-      { parse = map read . lines,
-        part1 = sum . map fuel,
-        part2 = sum . map totalFuel
-      }
+  Solution
+    { parse = map read . lines,
+      part1 = product . head . productPairs 2020,
+      part2 = product . head . productTriples 2020
+    }
 
-fuel :: Integer -> Integer
-fuel = max 0 . subtract 2 . (`div` 3)
+productPairs :: Integer -> [Integer] -> [[Integer]]
+productPairs target list = [[i, j] | (i : rest) <- tails list, j <- rest, i + j == target]
 
-totalFuel :: Integer -> Integer
-totalFuel = sum . takeWhile (> 0) . tail . iterate fuel
+productTriples :: Integer -> [Integer] -> [[Integer]]
+productTriples target list = [[i, j, k] | (i : is) <- tails list, (j : js) <- tails is, k <- js, i + j + k == target]
