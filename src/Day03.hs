dbdfc948ffc39d8e243d019ac79bda5e14429197
mru2 <david.ruyer+github@gmail.com>
2020-12-03 13:30:16 +0100

    day3 part2

--- src/Day03.hs
+++ src/Day03.hs
@@ -5,9 +5,13 @@ import Common
 solution =
   Solution
     { parse = map cycle . lines,
-      part1 = treeCount 3,
-      part2 = tbd
+      part1 = treeCount 3 1,
+      part2 = \input -> product ( map (\(right, down) -> treeCount right down input) [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] )
     }
 
-treeCount :: Int -> [[Char]] -> Int
-treeCount slope treeMap = countAll ('#' ==) . zipWith (!!) treeMap $ iterate (slope +) 0
+filterNth :: Int -> [a] -> [a]
+filterNth step (xs:rest) = xs:filterNth step filtered_rest where filtered_rest = drop (step - 1) rest
+filterNth _ [] = []
+
+treeCount :: Int -> Int ->  [[Char]] -> Int
+treeCount right down treeMap = length . filter ('#' ==) . zipWith (!!) (filterNth down treeMap) $ iterate (right +) 0
